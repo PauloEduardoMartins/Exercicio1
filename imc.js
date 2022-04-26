f0b4a34a42ce70bd87712e308aa5e94a2d6ca850
PauloEduardoMartins <pauloeduardomartinsdutra@gmail.com>
2022-04-26 09:54:37 -0300

    Projeto1

---
+++ imc.js
@@ -0,0 +1,14 @@
+function calcular(){
+
+
+    let um;
+    let dois;
+    let result;
+
+    um=document.getElementById("um").value;
+    dois=document.getElementById("dois").value;
+
+    result=um-dois;
+
+    alert("O resultado é " + result)
+}
